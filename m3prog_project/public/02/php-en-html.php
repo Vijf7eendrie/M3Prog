ef809a0bdc7899f5b779b529e15f4846a3818eaf
rayan <rayanaghmer072@outlook.com>
2024-03-15 09:46:53 +0100

    variabelen

---
+++ m3prog_project/public/02/php-en-html.php
@@ -0,0 +1,23 @@
+
+<?php
+$titel = "Titel";
+$url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d82b2f9f-4ae0-436c-ab1c-777c8318e035/de69voq-619672bb-eed7-4f35-b35c-745f2c20cfae.png/v1/fit/w_828,h_1286/mario__png__by_kevinfortuna_de69voq-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTk4NyIsInBhdGgiOiJcL2ZcL2Q4MmIyZjlmLTRhZTAtNDM2Yy1hYjFjLTc3N2M4MzE4ZTAzNVwvZGU2OXZvcS02MTk2NzJiYi1lZWQ3LTRmMzUtYjM1Yy03NDVmMmMyMGNmYWUucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.bPecbetNLVlig-xBB3DG-BH8DKSbnl9tT_uRepIf1dg";
+$titelfoto = "Mario";
+$auteur = "Vijf7eendrie";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<main>
+<?php echo $titel;?>
+<img src=" <?php echo $url;?>"></img>
+<?php echo $titelfoto;?>
+<?php echo $auteur;?>
+</main>    
+</body>
+</html>
