47689fc374d268a7a8712971b5c6d74fb414c37e
rayan <rayanaghmer072@outlook.com>
2024-03-15 10:48:44 +0100

    nieuwebestanden

---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,13 @@
+<?php
+$celsius = 13;
+$fahrenheit = ($celsius*1.8 + 32);
+//(30°C x 1.8) + 32 = 86°F'
+
+ $celsius=13;
+echo "<p>$fahrenheit</p>";
+$kelvin = ($celsius+273.15);
+echo $kelvin;
+
+
+$celsius = ($kelvin-273.15);
+echo "<p>$celsius</p>";
